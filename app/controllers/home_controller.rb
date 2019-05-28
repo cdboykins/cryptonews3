a9eefa771ff85f8ee9f501d79bbbdad8887cd333
Carlos Boykins <carlosdboykins@icloud.com>
2019-05-28 17:51:04 -0400

    Added header coin data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,18 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-
+    #Grab News Data
     @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
+    #Grab Pricing Data
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XLM,MIOTA,USDT,TRX&tsyms=USD'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
+
+
   end
 
   def prices
