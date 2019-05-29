73b9682ddee10c43007ebee7329fce75a02f6d28
Carlos Boykins <carlosdboykins@icloud.com>
2019-05-29 05:42:58 -0400

    fixed routing errors and Bstraps

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
     #Grab Pricing Data
-    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XLM,MIOTA,USDT,TRX&tsyms=USD'
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS&tsyms=USD'
     @prices_uri = URI(@prices_url)
     @prices_response = Net::HTTP.get(@prices_uri)
     @prices = JSON.parse(@prices_response)
@@ -17,6 +17,18 @@ class HomeController < ApplicationController
   end
 
   def prices
+    require 'net/http'
+    require 'json'
+    
+    @symbol = params[:sym]
+    if @symbol
+    @symbol = @symbol.upcase
+    
+    
+    @quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+ @symbol +'&tsyms=USD'
+    @quote_uri = URI(@quote_url)
+    @quote_response = Net::HTTP.get(@quote_uri)
+    @quote = JSON.parse(@quote_response)
   end
-
+end
 end
